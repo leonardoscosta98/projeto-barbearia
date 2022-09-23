1e4b47233f05e26dc3b09cf2bab27943560cf8c0
Leonardo Costa <95354795+leonardoscosta98@users.noreply.github.com>
2022-09-23 12:26:56 -0300

    ajuste Heroku

--- barbearia_projeto/__init__.py
+++ barbearia_projeto/__init__.py
@@ -1,48 +1,30 @@
 from flask import Flask
 from decouple import config
-from flask_talisman import Talisman
 
-app = Flask(__name__)
-
-app.config.from_mapping(
+def create_app(test_config=None):   
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.config.from_mapping(
         SECRET_KEY=config("SECRET_KEY"),
         # store the database in the instance folder        
         DATABASE_HOST=config("DATABASE_HOST"),
         DATABASE_NAME=config("DATABASE_NAME"),
-        DATABASE_NAME_TEST=config("DATABASE_NAME_TEST", default=None),
         DATABASE_USER=config("DATABASE_USER"),
         DATABASE_PASSWORD=config("DATABASE_PASSWORD"),    
     )
 
-from barbearia_projeto import main
+    # register the database commands
+    from barbearia_projeto import db
 
-app.register_blueprint(main.bp)
+    db.init_app(app)
 
+    from barbearia_projeto import main
 
-# def create_app(test_config=None):   
-#     """Create and configure an instance of the Flask application."""
-#     app = Flask(__name__, instance_relative_config=True)
-#     Talisman(app, content_security_policy=None)
-#     app.config.from_mapping(
-#         SECRET_KEY=config("SECRET_KEY"),
-#         # store the database in the instance folder        
-#         DATABASE_HOST=config("DATABASE_HOST"),
-#         DATABASE_NAME=config("DATABASE_NAME"),
-#         DATABASE_NAME_TEST=config("DATABASE_NAME_TEST", default=None),
-#         DATABASE_USER=config("DATABASE_USER"),
-#         DATABASE_PASSWORD=config("DATABASE_PASSWORD"),    
-#     )
+    app.register_blueprint(main.bp)
 
-#     # register the database commands
-#     from barbearia_projeto import db
-
-#     db.init_app(app)
-
-#     from barbearia_projeto import main
-
-#     app.register_blueprint(main.bp)
-
-#     app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/", endpoint="index")
    
    
-#     return app
+    return app
+
+app = create_app()
